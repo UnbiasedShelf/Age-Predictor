3c14498a3f47ecda448ce899198e6cb51a6ef3cd
Vladislav <vandl3511@gmail.com>
2023-01-04 00:33:28 +0300

    implemented 1 MiB cahe for 1 day

--- app/src/main/java/com/unbiasedshelf/agepredictor/data/di/AppModules.kt
+++ app/src/main/java/com/unbiasedshelf/agepredictor/data/di/AppModules.kt
@@ -3,15 +3,20 @@ package com.unbiasedshelf.agepredictor.data.di
 import android.content.Context
 import android.content.SharedPreferences
 import com.unbiasedshelf.agepredictor.data.network.AgifyService
+import com.unbiasedshelf.agepredictor.data.network.CachingControlInterceptor
 import com.unbiasedshelf.agepredictor.data.repository.AgifyRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import javax.inject.Named
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -22,21 +27,29 @@ class AppModules {
 
     @Provides
     fun provideRetrofit(
-        @Named("BASE_URL") baseUrl: String
-    ): Retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    @Provides
-    fun provideAgifyService(
-        retrofit: Retrofit
-    ): AgifyService = retrofit.create(AgifyService::class.java)
-
-    @Provides
-    fun provideFavoriteSharedPreferences(
+        @Named("BASE_URL") baseUrl: String,
         @ApplicationContext application: Context
-    ): SharedPreferences = application.getSharedPreferences("favorites", Context.MODE_PRIVATE)
+    ): Retrofit {
+        val sizeOfCache = (1024 * 1024).toLong() // 1 MiB
+        val cache = Cache(File(application.cacheDir, "agify_cache"), sizeOfCache)
+        val client = OkHttpClient()
+            .newBuilder()
+            .cache(cache)
+            .addNetworkInterceptor(CachingControlInterceptor())
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    fun provideAgifyService(retrofit: Retrofit): AgifyService = retrofit.create(AgifyService::class.java)
+
+    @Provides
+    fun provideFavoriteSharedPreferences(@ApplicationContext application: Context): SharedPreferences =
+        application.getSharedPreferences("favorites", Context.MODE_PRIVATE)
 
     @Provides
     fun provideAgifyRepository(
---
+++ app/src/main/java/com/unbiasedshelf/agepredictor/data/network/CachingControlInterceptor.kt
@@ -0,0 +1,17 @@
+package com.unbiasedshelf.agepredictor.data.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+
+
+class CachingControlInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalResponse: Response = chain.proceed(chain.request())
+        val maxAge = 60 * 60 * 24 // 1 day in seconds
+        return originalResponse.newBuilder()
+            .header("Cache-Control", "max-age=$maxAge")
+            .build()
+    }
+}
